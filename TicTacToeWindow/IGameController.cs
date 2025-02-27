c5d2b658e5f9cef230dd15f1bcb253048954082b
LiubomyrFadieiev <helplessmirek@gmail.com>
2025-02-27 20:03:18 +0100

    Added interface

---
+++ TicTacToeWindow/IGameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Structs;
+
+namespace TicTacToeWindow
+{
+    interface IGameController
+    {
+        public void HandleMove(int row, int col);
+        public void ResetGame();
+        public Cell[,] GetBoard();
+        public Player GetCurrentPlayer();
+    }
+}
