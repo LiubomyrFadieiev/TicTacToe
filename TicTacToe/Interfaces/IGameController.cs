c5333c0bcce7a670b2b17de3f792a752409e6b05
LiubomyrFadieiev <helplessmirek@gmail.com>
2025-02-28 13:49:03 +0100

    Updated interface

---
+++ TicTacToe/Interfaces/IGameController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToe.Structs;
+
+namespace TicTacToe.Interfaces
+{
+    public interface IGameController
+    {
+        public void AddPlayer(string name, char symbol);
+        public void PlayGame();
+        public void ResetGame();
+        public void UpdateGame();
+        public bool HandleMove(int row, int col, Player? player = null);
+        public void SetComputerPlayer(bool useComputer);
+    }
+}
